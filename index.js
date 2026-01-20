fbe0ba01db39caf8b30079fa86601425c2338c8c
Adeeb-06 <mohammadadeeb886@gmail.com>
2026-01-20 11:23:10 +0600

    fsjk

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const blogs = require('./data/blogs.json');
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
+
+app.get('/', (req, res) => {
+  res.send('Hello, Hero Server!');
+});
+
+app.get("/blogs", (req, res) => {
+    res.send(blogs);
+});
+
+app.get("/blogs/:slug", (req, res) => {
+    const slug = req.params.slug;
+    const blog = blogs.find(b => b.slug === slug);  
+    res.send(blog);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => { 
+    console.log(`Server is running on http://localhost:${PORT}`);
+ })
